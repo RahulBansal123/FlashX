51a802e7851bcbc89929759d17e8c623aaba5b0c
RahulBansal123 <rbbansal558@gmail.com>
2024-01-14 21:14:58 +0530

    removing dev breakpoints

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -13,18 +13,6 @@ export default function Home() {
         Stop missing out. Join the future of crypto trading.
       </div>
       <Guide />
-      <div className="hidden sm:block md:hidden lg:hidden xl:hidden bg-white rounded-full text-base fixed bottom-10 right-5 p-1">
-        sm
-      </div>
-      <div className="hidden sm:hidden md:block lg:hidden xl:hidden bg-white rounded-full text-base fixed bottom-10 right-5 p-1">
-        md
-      </div>
-      <div className="hidden sm:hidden md:hidden lg:block xl:hidden bg-white rounded-full text-base fixed bottom-10 right-5 p-1">
-        lg
-      </div>
-      <div className="hidden sm:hidden md:hidden lg:hidden xl:block bg-white rounded-full text-base fixed bottom-10 right-5 p-1">
-        xl
-      </div>
     </main>
   );
 }
